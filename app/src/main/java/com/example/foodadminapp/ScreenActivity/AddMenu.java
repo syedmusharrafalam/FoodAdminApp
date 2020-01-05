aac68f9eb28bf04c03d9868340b6976d073685af
Syed Musharraf Alam <musharrafslash@gmail.com>
2020-01-05 19:09:05 +0500

    first commit

---
+++ app/src/main/java/com/example/foodadminapp/ScreenActivity/AddMenu.java
@@ -0,0 +1,16 @@
+package com.example.foodadminapp.ScreenActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodadminapp.R;
+
+public class AddMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_menu);
+    }
+}
